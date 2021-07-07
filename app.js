df43e39a3e4737ad24a8187d7dc7e45c00766f18
Kolade <chrisjay967@gmail.com>
2021-07-08 00:48:10 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const number = document.querySelector(".number");
+const decrease = document.querySelector(".decrease");
+const increase = document.querySelector(".increase");
+
+// Initialize counter
+let count = 0;
+
+// Increase function
+decrease.addEventListener("click", () => {
+  count++;
+  updateDisplay();
+
+  //   Return count to 0
+  if (count === 50) {
+    window.location.reload();
+  }
+});
+
+// Decrease function
+increase.addEventListener("click", () => {
+  count--;
+  updateDisplay();
+
+  //   Return count to 0
+  if (count === 50) {
+    window.location.reload();
+  }
+});
+
+const updateDisplay = () => {
+  number.innerHTML = count;
+};
